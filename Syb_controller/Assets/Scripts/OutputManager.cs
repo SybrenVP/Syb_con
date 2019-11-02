d10639df6c6f675e0f017f8879ec0e372cfc2228
Sybren Van Praet <sybren.vp@gmail.com>
2019-11-02 10:05:16 +0100

    Look at gyro possibilities
    using Unity Remote 5

---
+++ Syb_controller/Assets/Scripts/OutputManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutputManager : MonoBehaviour
+{
+    private bool _gyroEnabled;
+    private Gyroscope _deviceGyro;
+
+    private void Start()
+    {
+        _gyroEnabled = EnableGyro();
+    }
+
+    private bool EnableGyro()
+    {
+        if(SystemInfo.supportsGyroscope)
+        {
+            _deviceGyro = Input.gyro;
+            _deviceGyro.enabled = true;
+            return true;
+        }
+        return false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(SystemInfo.supportsGyroscope)
+        {
+            Quaternion deviceRot = _deviceGyro.attitude;
+            Debug.Log(deviceRot.eulerAngles);
+        }
+
+        
+    }
+}
